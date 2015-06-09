826308b1dabad6fb25b99df5dcbc9b98ecc0214c
charlie wang <cwang8820@gmail.com>
2015-06-08 18:09:33 -0700

    edited mybind

--- myBind.js
+++ myBind.js
@@ -1,16 +1,38 @@
-Function.prototype.myBind = function(context)
-{
-	var fn = this
-	return function(){fn.apply(context)}
-}
+Function.prototype.myBind = function (context) {
+  var fn = this;
 
-var Cat = function (name) {
-  this.name = name;
-}
+  // arguments will be reset, so we need to save it. We also lop off
+  // the initial context argument.
+  var bindTimeArgs = Array.prototype.slice.call(arguments, 1);
 
-Cat.prototype.meow = function () {
-  console.log(this.name + " says meow!");
+  return function () {
+    var callTimeArgs = Array.prototype.slice.call(arguments);
+    return fn.apply(context, bindTimeArgs.concat(callTimeArgs));
+    // return fn.apply(context, callTimeArgs);
+
+  };
 };
 
-var curie = new Cat("Curie")
-setTimeout(curie.meow.myBind(curie), 1000);
+function Cat(name) {
+  this.name = name;
+};
+
+Cat.prototype.says = function (sound) {
+  console.log(this.name + " says " + sound + "!");
+}
+
+markov = new Cat("Markov");
+breakfast = new Cat("Breakfast");
+
+markov.says("meow");
+// Markov says meow!
+
+markov.says.myBind(breakfast, "meow")("haha");
+// Breakfast says meow!
+
+markov.says.myBind(breakfast)("meow");
+// Breakfast says meow!
+
+var notMarkovSays = markov.says.myBind(breakfast);
+notMarkovSays("meow");
+// Breakfast says meow!
