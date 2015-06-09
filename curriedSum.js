66a48cc005bee3fc3d617d6dcb34831a7338071c
charlie wang <cwang8820@gmail.com>
2015-06-08 19:56:20 -0700

    learned abour curry functions

---
+++ curriedSum.js
@@ -0,0 +1,48 @@
+function curriedSum (numArgs) {
+  var numbers = [];
+
+  function _curriedSum (num) {
+    numbers.push(num);
+
+    if (numbers.length == numArgs) {
+      var total = 0;
+
+      numbers.forEach(function (num) { total += num });
+      return total;
+    } else {
+      return _curriedSum;
+    }
+  }
+
+  return _curriedSum;
+}
+
+// console.log(curriedSum(2)(1)(2))
+
+Function.prototype.curry = function(numArgs)
+{
+	var fn = this
+	var args = []
+	function _curry(arg)
+	{
+		args.push(arg)
+		if (args.length === numArgs)
+		{
+			return fn.apply(null,args)
+		}
+		else{
+			return _curry
+		}
+	}
+	return _curry
+}
+
+function sumThree(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+
+
+var f1 = sumThree.curry(3);
+var f2 = f1(4);
+var f3 = f2(20);
+console.log(f3(6)); // = 30
