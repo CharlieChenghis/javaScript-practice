3bd3d932fd4ed3dd51b1c6d37caf1f53998c3d53
charlie wang <cwang8820@gmail.com>
2016-01-20 17:03:13 -0800

    more enumeration methods

--- enumerationPractice.js
+++ enumerationPractice.js
@@ -5,16 +5,30 @@ var my_each = function(array,f){
 }
 
 var square = function(el){
-	console.log(el*el);
+	// console.log(el*el);
 	return el*el;
 }
 
-my_each([1,2,3,4,5],square);
+// my_each([1,2,3,4,5],square);
 
 Array.prototype.myEach = function(f){
 	for(var j = 0; j<this.length; j++){
 		f(this[j]);
 	}
+	return this;
 };
 
-[2,4,5,6,7].myEach(square);
+// [2,4,5,6,7].myEach(square);
+
+Array.prototype.myMap = function(f){
+	var mappedArray = [];
+	this.myEach(function(el){
+		mappedArray.push(f(el));
+	});
+	return mappedArray;
+}
+
+var y = [1,2,3,10];
+var z = y.myMap(square);
+console.log(z);
+
